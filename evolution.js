c3d902df3189219ec5ef57737fe37ca8b5c759cf
riri sato <lilysatoo@gmail.com>
2013-04-30 18:13:29 +0900

    動物を一匹追加

--- evolution.js
+++ evolution.js
@@ -13,11 +13,13 @@ function drawWorld(){
 	var line="";
 	var worldArray=new Array();
 
-
 	for(var y=0;y<30;y++){
 		worldArray[y]=new Array();	
 		for(var x=0;x<100;x++){
 			worldArray[y][x]=plants[y][x];
+			if(animals[y][x]==="M"){
+				worldArray[y][x]=animals[y][x];
+			}
 			line+=worldArray[y][x];	
 		}
 		line+="\n";
@@ -48,3 +50,50 @@ function addPlants(){
 	plants[j][i]="*";
 	
 }
+
+var animals=new Array();
+for(var y1=0;y1<30;y1++){
+	animals[y1]=new Array();	
+	for(var x1=0;x1<100;x1++){
+		animals[y1][x1]=".";	
+	}
+}
+animals[15][50]="M";
+
+/*
+//方角
+var direction=Math.floor(Math.random()*8);
+
+function move(){
+	//居る場所
+	//var position=new
+	var m
+
+
+	switch(direction){
+		case 0://左上
+			position[m][n]=;
+			break;
+		case 1://上
+			
+			break;
+		case 2://右上
+		
+			break;
+		case 3://右
+			
+			break;
+		case 4://右下
+			
+			break;
+		case 5:
+		
+			break;
+		case 6:
+		
+			break;
+		case 7:
+			
+			break;
+	}
+}*/
